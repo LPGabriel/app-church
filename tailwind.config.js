135d194fd2b698e8e9306b70d4d3c01e723c8eeb
LPGabriel <gabrielvinilima2@gmail.com>
2022-11-29 18:42:02 -0300

    Emplementaçãop de animações e cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        azul: {
+          claro: '#C5DFFF',
+          escuro: '#061E3C',
+          hover: '#1057B0'
+        },
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+      },
+      keyframes: {
+        sino_kf: {
+          '0%, 100%': {
+            transform: 'rotate(-10deg)'
+          },
+          '50%': {
+            transform: 'rotate(10deg)'
+          }
+        }
+      },
+      animation: {
+        sino: 'sino_kf 0.31s ease-in-out infinite'
+      }
+    }
   },
   plugins: [
     require("daisyui"),
